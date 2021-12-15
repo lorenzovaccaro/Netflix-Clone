3d449ec81152266a02afcf9a074cee2ac6d605c4
Lorenzo Vaccaro <lorenzo.vaccaro1@gmail.com>
2021-12-14 23:49:43 -0500

    Initial Commit

---
+++ Shared/Netflix_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Netflix_CloneApp.swift
+//  Shared
+//
+//  Created by Lorenzo Vaccaro on 12/14/21.
+//
+
+import SwiftUI
+
+@main
+struct Netflix_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
